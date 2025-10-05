8d80da7dc2ac097fb56542a232137dce531f8cad
Angoty <angotyrabarijaona@gmail.com>
2025-10-05 19:27:57 +0300

    Initial commit-with-tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="bg-red-500 text-white p-10 text-center text-2xl font-bold">
+      Tailwind fonctionne !
+    </div>
+  );
+}
