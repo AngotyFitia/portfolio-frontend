f025a67bb5faa777ea7de8027f563f003382906e
Angoty <angotyrabarijaona@gmail.com>
2025-11-09 20:41:35 +0300

    updating-homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,11 @@
-import Description from "../components/Description"
-import Header from "../components/Header"
-import Projets from "../components/Projets"
+
 import Maintenance from "../components/Maintenance"
 
 export default function Home() {
   return (
     <main>
       <Maintenance />
-      {/* <Header />
-      <Description />
-      <Projets /> */}
+      
     </main>
   )
 }
