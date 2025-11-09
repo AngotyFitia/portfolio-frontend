c20a52d0fcc2b6c7ab220732d719e72d13c3dec1
Angoty <angotyrabarijaona@gmail.com>
2025-11-09 20:46:38 +0300

    switching-page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,10 @@
-import Description from "../components/Description"
-import Header from "../components/Header"
-import Projets from "../components/Projets"
+
 import Maintenance from "../components/Maintenance"
 
 export default function Home() {
   return (
     <main>
       <Maintenance />
-      {/* <Header />
-      <Description />
-      <Projets /> */}
     </main>
   )
 }
